158c1b2708bad4ccb67d0e32da093cef0bcb4892
mahdis slvt <mahdisslvt961@gmail.com>
2023-12-16 10:21:53 +0330

    remove repeated numbers in an array

---
+++ repeatednumber.js
@@ -0,0 +1,14 @@
+const arr=[1,3,4,8,10,4,12,1,19]
+const newarr=[]
+const repeatedarr=[]
+
+for(let i=0 ; i<=arr.length ; i++){
+    if(newarr.indexOf(arr[i]) === -1){
+        newarr.push(arr[i])
+    }
+    
+    if(arr.indexOf(arr[i]) !== ){
+
+    }
+}
+console.log(newarr)
