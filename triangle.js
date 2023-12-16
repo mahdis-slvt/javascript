b6846d5d553c9b0b292bb1da1ec2bfc1465f8978
mahdis slvt <mahdisslvt961@gmail.com>
2023-12-16 10:24:34 +0330

    diagnose kind of triangle with if & else if

---
+++ triangle.js
@@ -0,0 +1,11 @@
+const x=9;
+const y=9;
+const z=11;
+
+if(x==y==z){
+    console.log("parallelogram")
+}else if(x==y || x==z || y==z){
+    console.log("isosceles")
+}else{
+    console.log("unequal traingle")
+}
