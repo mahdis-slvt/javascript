714609a919405b66ea6594d5e4424bd2e542f466
mahdis slvt <mahdisslvt961@gmail.com>
2023-12-21 10:20:30 +0330

    use functions

--- rock.js
+++ rock.js
@@ -1,34 +1,41 @@
 const choices = ["rock", "paper", "scissors"];
 
-const userChoise=prompt("please enter rock,paper or scissors");
-console.log(userChoise);
-if(userChoise){
-    console.log(`you chose ${userChoise}`)
-}else{
-    console.log(`you cheated`)
-}
-
-const computerChoice=choices[Math.floor(Math.random()*3)];
-console.log(`computer chose ${computerChoice}`);
-
-if(userChoise == computerChoice){
-    console.log(`you are equal!`)
-}else if( userChoise == "rock"){
-    if(computerChoice == "paper"){
-        console.log("you loose");
-    }else{
-        console.log("you wone");
-    }
-}else if( userChoise == "paper"){
-    if(computerChoice == "scissors"){
-        console.log("you loose");
-    }else{
-        console.log("you wone");
-    }
-}else if( userChoise == "scissors"){
-    if(computerChoice == "rock"){
-        console.log("you loose");
-    }else{
-        console.log("you wone");
+const checkWinner = (player , computer) => {
+    if(player == computer){
+        console.log(`you are equal!`)
+    }else if( player == "rock"){
+        if(computer == "paper"){
+            console.log("you loose");
+        }else{
+            console.log("you wone");
+        }
+    }else if( player == "paper"){
+        if(computer == "scissors"){
+            console.log("you loose");
+        }else{
+            console.log("you wone");
+        }
+    }else if( player == "scissors"){
+        if(computer == "rock"){
+            console.log("you loose");
+        }else{
+            console.log("you wone");
+        }
     }
 }
+
+const play= () => {
+    const userChoise=prompt("please enter rock,paper or scissors");
+    if(userChoise){
+        console.log(`you chose ${userChoise}`)
+    }else{
+        console.log(`you cheated`)
+    }
+
+    const computerChoice=choices[Math.floor(Math.random()*3)];
+    console.log(`computer chose ${computerChoice}`);
+
+    const result=checkWinner(userChoise, computerChoice)
+}
+play()
+
